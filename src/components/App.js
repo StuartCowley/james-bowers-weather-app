b3ed315f6559a6f03d0ceeb4ddc6439d92f9a1bc
James Bowers <jamesbowers123@gmail.com>
2022-05-26 11:02:13 +0100

    add LocationDetails

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import "../styles/App.css";
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+
+function App(props) {
+  return (
+    <div className="App">
+      <LocationDetails
+        city={props.location.city}
+        country={props.location.country}
+      />
+    </div>
+  );
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
