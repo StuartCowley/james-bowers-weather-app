3fabf1058f87efadbc8e1a2b0eb8b9e66b7a2a60
James Bowers <jamesbowers123@gmail.com>
2022-05-27 12:51:38 +0100

    Add forecastSummaries

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -2,20 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ForecastSummary(props) {
-  const { date, temperature, description, icon } = props;
+  const { date, description, icon, temperature } = props;
   return (
-    (<div className="date">{date}</div>),
-    (<div className="maxTemp">{temperature.max}</div>),
-    (<div className="description">{description}</div>),
-    (<div className="icon">{icon}</div>)
+    <div className="forecast-summary">
+      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__description">{description}</div>
+      <div className="forecast-summary__icon">{icon}</div>
+      <div className="forecast-summary__maxTemp">{temperature.max}</div>
+    </div>
   );
 }
 
 ForecastSummary.propTypes = {
-  date: PropTypes.string.isRequired,
-  temperature: PropTypes.number.isRequired,
+  date: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-  icon: PropTypes.number.isRequired,
+  icon: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
 };
 
 export default ForecastSummary;
