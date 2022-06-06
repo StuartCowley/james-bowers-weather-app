0b25254fa89f66ad72ff03367862071d9cd405ce
James Bowers <jamesbowers123@gmail.com>
2022-06-06 15:09:33 +0100

    add Search feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "../styles/App.css";
 import React, { useEffect, useState } from "react";
 import LocationDetails from "./LocationDetails";
+import SearchForm from "./SearchForm";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
@@ -9,6 +10,11 @@ function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
+
+  const handleCitySearch = () => {
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  };
 
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
@@ -19,12 +25,17 @@ function App() {
   };
 
   useEffect(() => {
-    getForecast(setSelectedDate, setForecasts, setLocation);
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
   }, []);
 
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
